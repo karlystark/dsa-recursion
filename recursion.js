15ebe095a303ff00815659d851992d8f67755849
Karly Stark <karlystark@gmail.com>
2023-10-03 12:31:31 -0700

    tests passing up until isPalindrome

--- recursion.js
+++ recursion.js
@@ -1,25 +1,53 @@
 /** product: calculate the product of an array of numbers. */
 
+// [1,2,3]
 function product(nums) {
+  if(nums.length === 0){
+    return 1;
+  }
+
+  return nums[0] * product(nums.slice(1));
 
 }
 
 /** longest: return the length of the longest word in an array of words. */
-
+//["hello", "hi", "hola"]
 function longest(words) {
+  if(words.length === 0){
+    return 0;
+  }
+
+  let currWordLength = words[0].length;
+
+  return Math.max(currWordLength, longest(words.slice(1)));
 
 }
 
 /** everyOther: return a string with every other letter. */
 
+//["hello"] => "hlo"
 function everyOther(str) {
+  if(str.length === 0){
+    return "";
+  }
 
+  return str[0] + everyOther(str.slice(2));
 }
 
 /** find: return boolean depending on if val exists in array or not. */
-
+//["duck", "cat", "pony"], "cat" => true
+//["duck", "cat", "pony"] "porcupine" => false
 function find(arr, val) {
 
+if(arr.length === 0){
+  return false;
+}
+
+if(arr[0] === val){
+  return true;
+}
+
+return find(arr.slice(1), val);
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
